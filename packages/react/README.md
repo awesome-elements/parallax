f29024d728e178959ce17ef77c3559382aeb156f
Sean Wong <seanwong24@live.com>
2021-12-12 18:58:37 -0600

    add react package

---
+++ packages/react/README.md
@@ -0,0 +1,2 @@
+# Awesome-Elements Parallax
+React specific wrapper for [@awesome-elements/parallax](https://www.npmjs.com/package/@awesome-elements/parallax).
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,11 +1,20 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'awesome-elements',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@awesome-elements/parallax',
+      proxiesFile: './packages/react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-readme',
