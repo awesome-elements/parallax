c77824d7a8a6f6bd898701cdee0498debebb873e
Sean Wong <seanwong24@live.com>
2021-12-12 19:00:17 -0600

    add publish.sh

---
+++ publish.sh
@@ -0,0 +1,19 @@
+echo "New version: "
+echo "[<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease [--preid=<prerelease-id>] | from-git]"
+read NEW_VERSION
+npm version $NEW_VERSION
+npm publish
+
+CORE_PROJECT_VERSION=$(npm -s run env echo '$npm_package_version')
+CORE_PROJECT_PATH=$(pwd)
+REACT_PROJECT_PATH=./packages/react
+
+cd $REACT_PROJECT_PATH
+if npm i --save-exact @awesome-elements/parallax@$CORE_PROJECT_VERSION ; then
+    npm version $CORE_PROJECT_VERSION
+    git add package*.json
+    git commit -m "react package updated to $CORE_PROJECT_VERSION"
+    npm publish
+else
+    echo "Failed to fetch the new version of core package."
+fi
